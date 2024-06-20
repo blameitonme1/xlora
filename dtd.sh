a82328d66ca800e02c89c96bb73b92947000cbab
blameitonme1 <2741122036@qq.com>
2024-06-21 01:29:53 +0800

    save code

---
+++ dtd.sh
@@ -0,0 +1,12 @@
+CUDA_VISIBLE_DEVICES=0 python main.py \
+    --model-name-or-path google/vit-base-patch16-224-in21k \
+    --dataset-name dtd \
+    --mode full \
+    --n_frequency 3000 \
+    --num_epochs 10 \
+    --n_trial 1 \
+    --head_lr 1e-3 \
+    --weight_decay 7e-5 \
+    --fft_lr 3e-1 \
+    --scale 300.0 \
+    --share_entry 
